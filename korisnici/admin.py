308c37cf4430fce5b9ce54aadcf79690cf50b755
markoMaksimovic1419S <mmaksimovic1419s@raf.rs>
2020-11-22 18:20:54 +0100

    Added models

--- korisnici/admin.py
+++ korisnici/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from korisnici.models import Korisnik
 # Register your models here.
+admin.site.register(Korisnik)
--- korisnici/models.py
+++ korisnici/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+class Korisnik(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    ime = models.CharField(max_length=100, blank=False, null=False)
+    prezime = models.CharField(max_length=100, blank=False, null=False)
+    datum_rodjenja = models.DateField(blank=False, null=False)
+    vreme_kreiranja_naloga = models.DateTimeField(default=timezone.now)
+    adresa = models.CharField(max_length=200, blank=False, null=False)
+
+
